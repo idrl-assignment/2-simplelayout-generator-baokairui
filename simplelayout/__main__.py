2f6ba90b81e404ceddbb729484429cf0c64ea2c6
包凯瑞 <673575209@qq.com>
2020-11-18 18:35:25 +0800

    init修改

--- simplelayout/__main__.py
+++ simplelayout/__main__.py
@@ -1,5 +1,5 @@
 # TODO 正确导入函数 generate_matrix, save_matrix, save_fig
-from simplelayout.cli.cli_generate import get_options  # TODO: 保证不修改本行也可以正确导入
+from simplelayout.cli import get_options  # TODO: 保证不修改本行也可以正确导入
 from simplelayout.generator.core import generate_matrix
 from simplelayout.generator.utils import save_matrix
 from simplelayout.generator.utils import save_fig
